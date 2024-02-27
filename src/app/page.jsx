5df1ab2af7c2b91ad78983dcd83179a10a257c2b
Agadyr <kairambekovmadi@gmail.com>
2024-02-27 21:17:29 +0600

    added bootstrap and header component

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import Header from "@/components/header"
+import Link from "next/link"
+
+export default function Main(){
+    return (
+        <div>
+            <Header/>
+            <section className="auth-token">
+
+            </section>
+        </div>
+    )
+}
