f56575a2e06f19b8f4d3c4f3db0cfdd578126334
Agadyr <kairambekovmadi@gmail.com>
2024-02-27 21:32:15 +0600

    made add token form for authorize user

---
+++ src/app/home/page.jsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Header from "@/components/header"
+
+export default function Home(){
+    return (
+        <div>
+            <Header/>
+            <div className="py-5"></div>
+            <div className="ai">
+                <div className="ai-tems">
+
+                </div>
+                <div className="ai-tems">
+
+                </div>
+                <div className="ai-tems">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,35 @@
 'use client'
 import Header from "@/components/header"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 export default function Main(){
+    const [input, setInput] = useState("")
+    const [error, setError] = useState("")
+    const router = useRouter()
+    const save = () => {
+        if(input.length <= 5){
+            setError("Token field is required")
+        }else{
+            router.push("/home")
+        }
+    }
     return (
         <div>
             <Header/>
             <section className="auth-token">
-
+                <div className="py-5">
+                </div>
+                <form action="">
+                    <h1>Set your Token</h1>
+                    <div className="form-floating">
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" id="token" className="form-control" placeholder="" />
+                        <label htmlFor="token">Your token</label>
+                        {error.length >= 1 && <p className="text-danger">{error}</p>}
+                        <button type="button" className="btn btn-primary w-100 mt-3" onClick={() => save()}>Next</button>
+                    </div>
+                </form>
             </section>
         </div>
     )
